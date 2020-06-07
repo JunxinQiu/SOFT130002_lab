bc1c1f37eb0d33fde13bf0fe3d98e034a197939e
JunxinQiu <55233025+JunxinQiu@users.noreply.github.com>
2020-06-07 21:53:48 +0800

    Add files via upload

---
+++ lab10/config.php
@@ -0,0 +1,5 @@
+<?php define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'testuser');
+define('DBPASS', 'testuser');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;'); ?>
